c43f2b12d2b86672c33a46959b23b9280d94c96c
lksoulman <lksoulman@163.com>
2018-02-08 14:24:38 +0800

    修改文件位置

---
+++ ShareUnit/Interfaces/AsfCache/CacheType.pas
@@ -0,0 +1,27 @@
+unit CacheType;
+
+////////////////////////////////////////////////////////////////////////////////
+//
+// Description£∫
+// Author£∫      lksoulman
+// Date£∫        2017-8-12
+// Comments£∫
+//
+////////////////////////////////////////////////////////////////////////////////
+
+interface
+
+uses
+  Windows,
+  Classes,
+  SysUtils;
+type
+
+  // ª∫¥Ê¿‡–Õ
+  TCacheType = (ctBaseData,
+                ctUserData
+                );
+
+implementation
+
+end.
---
+++ ShareUnit/Interfaces/AsfCache/UserCache.pas
@@ -0,0 +1,49 @@
+unit UserCache;
+
+////////////////////////////////////////////////////////////////////////////////
+//
+// Description£º UserCache Interface
+// Author£º      lksoulman
+// Date£º        2017-8-11
+// Comments£º
+//
+////////////////////////////////////////////////////////////////////////////////
+
+interface
+
+uses
+  Windows,
+  Classes,
+  SysUtils,
+  WNDataSetInf;
+
+const
+  UPLOADVALUE_MODIFY = 1;       // ÐÞ¸Ä
+  UPLOADVALUE_DELETE = 2;       // É¾³ý
+
+type
+
+  // UserCache Interface
+  IUserCache = Interface(IInterface)
+    ['{D3E280F2-E5F1-4D74-818B-1F0BFC0016AE}']
+    // StopService
+    procedure StopService;
+    // UpdateTables
+    procedure UpdateTables;
+    // InitUpdateTables
+    procedure InitUpdateTables;
+    // ReplaceCreateCacheTables
+    procedure ReplaceCreateCacheTables;
+    // ExecuteSql
+    procedure ExecuteSql(ATable, ASql: string);
+    // SyncQuery
+    function SyncQuery(ASql: WideString): IWNDataSet;
+    // UpdateVersion
+    function GetUpdateVersion(ATable: string): Integer;
+    // AsyncQuery
+    procedure AsyncQuery(ASql: WideString; ADataArrive: Int64; ATag: Int64);
+  end;
+
+implementation
+
+end.
